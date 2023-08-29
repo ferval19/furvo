c5e5d31938961c370d5815dc9901ca0148c11452
Fernando Jesús Fernández Valero <ferval19@gmail.com>
2023-08-29 18:05:21 +0200

    -fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "61161f43277ff4db"; }
+/******/ 		__webpack_require__.h = function() { return "1bafffbf42d258fd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
